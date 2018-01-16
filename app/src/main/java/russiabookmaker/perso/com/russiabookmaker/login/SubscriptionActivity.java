fc75f51a1ea34e99c46751f2a75ee5c87e03da8f
lassayah <laurene.assayah@gmail.com>
2018-01-16 17:25:02 +0100

    subscription

--- app/src/main/java/russiabookmaker/perso/com/russiabookmaker/login/SubscriptionActivity.java
+++ app/src/main/java/russiabookmaker/perso/com/russiabookmaker/login/SubscriptionActivity.java
@@ -1,30 +1,32 @@
 package russiabookmaker.perso.com.russiabookmaker.login;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import russiabookmaker.perso.com.russiabookmaker.HomeActivity;
 import russiabookmaker.perso.com.russiabookmaker.R;
+import russiabookmaker.perso.com.russiabookmaker.ranking.CurrentRankFragment;
+import russiabookmaker.perso.com.russiabookmaker.utils.AlertDialogFragment;
 
-public class SubscriptionActivity extends AppCompatActivity {
-
-    private TextView newUsername;
-    private TextView newPassword;
-    private TextView newPasswordConfirm;
-    private Button validateButton;
+public class SubscriptionActivity extends AppCompatActivity implements SubscriptionFragment.OnSubscriptionFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,52 +35,18 @@ public class SubscriptionActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        newUsername = (TextView) findViewById(R.id.newUsernameEdit);
-        newPassword = (TextView) findViewById(R.id.newPasswordEdit);
-        newPasswordConfirm = (TextView) findViewById(R.id.newPasswordConfirmEdit);
-        validateButton = (Button) findViewById(R.id.submitButton);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        SubscriptionFragment sbFragment = new SubscriptionFragment();
+        fragmentTransaction.add(R.id.subscription_container, sbFragment);
+        fragmentTransaction.commit();
 
-        validateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (newPassword.getText() == newPasswordConfirm.getText())
-                    callService();
-            }
-        });
 
     }
 
-    private void callService(){
-        SubscriptionService subscriptionService = SubscriptionService.retrofit.create(SubscriptionService.class);
-        final Call<User> call =
-                subscriptionService.callSusbcription(newUsername.getText().toString(), newPassword.getText().toString());
+    @Override
+    public void onSubscriptionFragmentInteraction(Uri uri) {
 
-        call.enqueue(new Callback<User>() {
-            @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                User user = response.body();
-                System.out.println("user : " + user.isLoggedIn());
-                if (user.isLoggedIn()) {
-                    Intent intent = new Intent(SubscriptionActivity.this, HomeActivity.class);
-                    SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(getString(R.string.login), Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPref.edit();
-                    editor.putString(getString(R.string.login), newUsername.getText().toString());
-                    editor.commit();
-                    startActivity(intent);
-                    finish();
-                }
-                else
-                {
-
-                }
-
-            }
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-                Log.d("callko", t.getMessage());
-                System.out.println(t.getCause());
-            }
-        });
     }
 
 }
--- app/src/main/java/russiabookmaker/perso/com/russiabookmaker/login/SubscriptionFragment.java
+++ app/src/main/java/russiabookmaker/perso/com/russiabookmaker/login/SubscriptionFragment.java
@@ -1,19 +1,36 @@
 package russiabookmaker.perso.com.russiabookmaker.login;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.TextView;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import russiabookmaker.perso.com.russiabookmaker.HomeActivity;
 import russiabookmaker.perso.com.russiabookmaker.R;
+import russiabookmaker.perso.com.russiabookmaker.utils.AlertDialogFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SubscriptionFragment.OnFragmentInteractionListener} interface
+ * {@link SubscriptionFragment.OnSubscriptionFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link SubscriptionFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -28,7 +45,14 @@ public class SubscriptionFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private TextView newUsername;
+    private TextView newPassword;
+    private TextView newPasswordConfirm;
+    private Button validateButton;
+    private View mProgressView;
+    private View mSubscriptionFormView;
+
+    private OnSubscriptionFragmentInteractionListener mListener;
 
     public SubscriptionFragment() {
         // Required empty public constructor
@@ -65,21 +89,54 @@ public class SubscriptionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subscription, container, false);
+        View v = inflater.inflate(R.layout.fragment_subscription, container, false);
+        newUsername = (TextView) v.findViewById(R.id.newUsernameEdit);
+        newPassword = (TextView) v.findViewById(R.id.newPasswordEdit);
+        newPasswordConfirm = (TextView) v.findViewById(R.id.newPasswordConfirmEdit);
+        validateButton = (Button) v.findViewById(R.id.submitButton);
+        mSubscriptionFormView = v.findViewById(R.id.subscription_form);
+        mProgressView = v.findViewById(R.id.subscription_progress);
+
+        newPasswordConfirm.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+
+                    attemptSubscription();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (newPassword.getText().toString().equals(newPasswordConfirm.getText().toString()))
+                    callService();
+                else
+                {
+                    showProgress(false);
+                    newPasswordConfirm.setError(getString(R.string.subscription_wrong_password));
+                    newPasswordConfirm.requestFocus();
+                }
+            }
+        });
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.onSubscriptionFragmentInteraction(uri);
         }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof OnSubscriptionFragmentInteractionListener) {
+            mListener = (OnSubscriptionFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -92,6 +149,97 @@ public class SubscriptionFragment extends Fragment {
         mListener = null;
     }
 
+    private void callService(){
+        SubscriptionService subscriptionService = SubscriptionService.retrofit.create(SubscriptionService.class);
+        final Call<User> call =
+                subscriptionService.callSusbcription(newUsername.getText().toString(), newPassword.getText().toString());
+
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                User user = response.body();
+                System.out.println("user : " + user.isLoggedIn());
+                if (user.isLoggedIn()) {
+                    Intent intent = new Intent(getActivity(), HomeActivity.class);
+                    SharedPreferences sharedPref = getContext().getSharedPreferences(getString(R.string.login), Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString(getString(R.string.login), newUsername.getText().toString());
+                    editor.commit();
+                    startActivity(intent);
+                    getActivity().finish();
+                }
+                else
+                {
+
+                }
+
+            }
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Log.d("callko", t.getMessage());
+                System.out.println(t.getCause());
+            }
+        });
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptSubscription() {
+        // Reset errors.
+        newUsername.setError(null);
+        newPassword.setError(null);
+        newPasswordConfirm.setError(null);
+
+        // Store values at the time of the login attempt.
+        String username = newUsername.getText().toString();
+        String password = newPassword.getText().toString();
+        String confirmPassword = newPasswordConfirm.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            newPassword.setError(getString(R.string.error_invalid_password));
+            focusView = newPassword;
+            cancel = true;
+        }
+
+        // Check for a valid password confirmation, if the user entered one.
+        if (!TextUtils.isEmpty(confirmPassword) && !isPasswordValid(confirmPassword)) {
+            newPasswordConfirm.setError(getString(R.string.error_invalid_password));
+            focusView = newPasswordConfirm;
+            cancel = true;
+        }
+
+        // Check for a valid username.
+        if (TextUtils.isEmpty(username)) {
+            newUsername.setError(getString(R.string.error_field_required));
+            focusView = newUsername;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            //showProgress(true);
+            //mAuthTask = new UserLoginTask(email, password);
+            //mAuthTask.execute((Void) null);
+        }
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 2;
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -102,8 +250,44 @@ public class SubscriptionFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface OnSubscriptionFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onSubscriptionFragmentInteraction(Uri uri);
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mSubscriptionFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mSubscriptionFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mSubscriptionFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mSubscriptionFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
 }
